7fb3fd29d111411eea7cf896d383bfbd41868eb0
Shweta <shwetakoffficial@gmail.com>
2024-09-27 17:26:49 +0530

    register user

--- backend/auth.controllers.js
+++ backend/auth.controllers.js
@@ -1,3 +1,7 @@
+const User=require("./user-model");
+
+
+
 const home=async(req ,res)=>{
     try {
         res.status(200).send('welcome home')
@@ -6,13 +10,40 @@ const home=async(req ,res)=>{
     }
 }
 
+//register logic
+/**
+ * 1.get registartion data: retrieve usename,email password
+ * 2.check email existence
+ * 3.hash password: secure
+ * 4.create user: create a new user using hashed password
+ * 5.save to db: save user to database
+ * 6.respond: respond with 'registartion successful
+ */
 
+//register
 const register=async(req,res)=>{
     try {
+
+        const {username,email,phone,password}=req.body;
+
+        //check email existence
+        const userExist= await User.findOne({email});
         console.log("body data",req.body);
-        res.status(200).json({message:req.body})
+
+        if(userExist){
+            return res.status(400).json({message:"email already exists"})
+        }
+
+        //hash passwrd
+        const hashPassword= await bcrypt.hash(password,10)
+        
+        //if not exist then create a user
+       const newUser= await User.create({username,email,password:hashPassword,phone})
+        res.status(201).json({ message: "User created successfully", user: newUser });
+
+
     } catch (error) {
-        console.log(error)
+       res.status(500).json('internal server error')
         
     }
 }
